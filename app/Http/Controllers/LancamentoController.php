c77fa7de045fe6711e8fa09058044d7735d5ae32
Paulo Henrique <paulo-henrique-morada@hotmail.com>
2022-09-19 20:16:44 -0300

    Listar lancamentos, função show, criação de lancamento resource collection

--- app/Http/Controllers/LancamentoController.php
+++ app/Http/Controllers/LancamentoController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Transformers\Lancamento\LancamentoResourceCollection;
+use App\Models\Lancamento;
+use App\Services\ResponseService;
 use Illuminate\Http\Request;
 
 class LancamentoController extends Controller
 {
-    //
+    private $lancamento;
+
+    public function __construct(Lancamento $lancamento){
+        $this->lancamento = $lancamento;
+    }
+
+    public function show(){
+        try {
+
+            $lancamento = $this->lancamento->paginate();
+
+        } catch (\Throwable|\Exception $e) {
+            return ResponseService::exception('produtos.show', null, $e);
+        }
+
+        return new LancamentoResourceCollection($lancamento);
+    }
 }
--- app/Models/Lancamento.php
+++ app/Models/Lancamento.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Lancamento extends Model
 {
     use HasFactory;
+
+    public function usuario(){
+        return $this->belongsTo(User::class, 'usuario_id', 'id');
+    }
+
+    public function funcionario(){
+        return $this->belongsTo(Funcionario::class, 'funcionario_id', 'id');
+    }
+
+    public function produto(){
+        return $this->belongsTo(User::class, 'produto_id', 'id');
+    }
 }
